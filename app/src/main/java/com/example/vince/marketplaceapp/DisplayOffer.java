189589e43f6cb35e66f725f926439050a23316cc
Neu-Usar <vincenthwang421@gmail.com>
2017-06-20 16:21:41 -0700

    Revert "Revert "Added "Display Offer" screen""
    This reverts commit 083c9d2d9e0a706c7b4a7d8cd46637b249338912.

---
+++ app/src/main/java/com/example/vince/marketplaceapp/DisplayOffer.java
@@ -0,0 +1,13 @@
+package com.example.vince.marketplaceapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayOffer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_offer);
+    }
+}
