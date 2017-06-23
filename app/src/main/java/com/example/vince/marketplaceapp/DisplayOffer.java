2a855b37f5a332232a0f9dabee007e83d16eff1c
Twang720 <timqwang@gmail.com>
2017-06-23 15:35:06 -0700

    first attempt to load images from firebase storage

--- app/src/main/java/com/example/vince/marketplaceapp/DisplayOffer.java
+++ app/src/main/java/com/example/vince/marketplaceapp/DisplayOffer.java
@@ -1,17 +1,27 @@
 package com.example.vince.marketplaceapp;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class DisplayOffer extends AppCompatActivity {
@@ -26,6 +36,10 @@ public class DisplayOffer extends AppCompatActivity {
 
     DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
     DatabaseReference nameRef;
+
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    StorageReference storageRef = storage.getReferenceFromUrl("gs://marketplaceapp-c265f.appspot.com/").child("image.jpg");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +72,22 @@ public class DisplayOffer extends AppCompatActivity {
                 if(store[4].equals("none")){
                     textViewPhone.setText("Not provided.");
                 }
+                try {
+                    final File localFile = File.createTempFile("images", "jpg");
+                    storageRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                            targetImage.setImageBitmap(bitmap);
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                        }
+                    });
+                } catch (IOException e ) {}
+
 
             }
             @Override
