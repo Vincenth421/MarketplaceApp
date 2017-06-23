52f53951c2e526d1b44f3e264f49dd84b5f142e0
Vincent Hwang <Vincenth421@users.noreply.github.com>
2017-06-23 15:38:19 -0700

    no more duplicates

--- app/src/main/java/com/example/vince/marketplaceapp/MainActivity.java
+++ app/src/main/java/com/example/vince/marketplaceapp/MainActivity.java
@@ -59,7 +59,7 @@ public class MainActivity extends Activity  {
     @Override
     protected void onStart() {
         super.onStart();
-
+        listings.removeAllViews();
         rootRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
