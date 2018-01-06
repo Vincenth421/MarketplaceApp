fba91878b03908abde4d596d98ae371319d515e9
Vincent Hwang <vincenthwang421@gmail.com>
2018-01-06 14:46:56 -0800

    Revert "Revert "fragments again""
    This reverts commit aa4d009d2a4a05cc346e81bd58296594c5f904e5.

--- app/src/main/java/com/example/vince/marketplaceapp/OfferFragment.java
+++ app/src/main/java/com/example/vince/marketplaceapp/OfferFragment.java
@@ -5,6 +5,7 @@ import android.media.Image;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,16 +15,16 @@ import android.widget.TextView;
  * Created by vince on 12/25/2017.
  */
 
-public class OfferFragment extends Fragment{
+public class OfferFragment extends FragmentActivity{
 
-    TextView title = (TextView) getView().findViewById(R.id.textTitle);
-    TextView price = (TextView) getView().findViewById(R.id.editTextPrice);
+    TextView title = (TextView) findViewById(R.id.textTitle);
+    TextView price = (TextView) findViewById(R.id.editTextPrice);
 
-    @Override
+    /*@Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.offer_layout,container,false);
         return view;
-    }
+    }*/
 
     public void setTitle(String str){
         title.setText(str);
